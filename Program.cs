def9b402baa8ce8ac83d42aff7a164ef20451d33
Erik Mortimer <erik.mortimer@outlook.com>
2021-07-12 21:54:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
